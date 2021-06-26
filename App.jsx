f088c521b4f217788c85d75b1775edb823415692
Akash-Kumar-26 <85391428+Akash-Kumar-26@users.noreply.github.com>
2021-06-26 17:22:46 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Inof from "./Info"
+function App() {
+
+  return (
+   <div>
+    <Header />
+    <Footer />
+    <Inof />
+    <Inof />
+    <Inof />
+   </div>
+  );
+}
+export default App;
